4fe2ffc3abab5a352342eb9baa7117baf8e6ed17
VictorAgosta <107890899+VictorAgosta@users.noreply.github.com>
2022-08-24 15:30:11 -0300

    Update mobiles.py

--- mobiles.py
+++ mobiles.py
@@ -1,3 +1,11 @@
+from tkinter import ttk
+import pandas as pd
+from tkinter import *
+from tkinter import messagebox
+from datetime import date
+import tkinter.font as tk_font
+from mensagens import msg_erro_cadastro_apag, msg_erro_cadastro, msg_erro_apagar
+
 
 ######################################### Cadastro mobiles #########################################
 
@@ -33,48 +41,70 @@ def cadastro_mobile(cod_mob, obj_mob):
     plan0_chip = pd.read_excel('Inventario_chip.xlsx', sheet_name='chip')
 
     # criação da interface cadastro pessoas
-    app3 = Toplevel()
+    app3 = Tk()
     app3.title('Cadastro Mobiles')
-    app3.geometry('500x500')
+    app3.geometry('500x460')
     app3.configure(background='Silver')
 
-    lb0 = Label(app3, text='Código Mobile', background='Silver', foreground='black', anchor=W)
-    lb0.place(x=10, y=30, height=20, width=100)
+    font_titulo = tk_font.Font(family="Lucida Grande", size=15)
 
-    nome_mobile = Label(app3, text=f'{cod_mob}', background='Silver', foreground='black', anchor=W)
-    nome_mobile.place(x=120, y=30, height=20, width=350)
+    # Titulo tela
 
-    # nome mobile:
-    lb1 = Label(app3, text='Equipamento:', background='Silver', foreground='black', anchor=W)
+    lb0 = Label(app3, text='Cadastro dos Equipamentos Mobile:', background='Silver',
+                foreground='black', font=font_titulo)
+
+    lb0.place(x=0, y=10, height=30, width=500)
+
+    lb1 = Label(app3, text='Código Mobile', background='Silver', foreground='black', anchor=W)
     lb1.place(x=10, y=60, height=20, width=100)
 
-    nome_mobile = Label(app3, text=f'{obj_mob}', background='Silver', foreground='black', anchor=W)
+    nome_mobile = Label(app3, text=f'{cod_mob}', background='Silver', foreground='black', anchor=W)
     nome_mobile.place(x=120, y=60, height=20, width=350)
 
-    # marca
-    lb2 = Label(app3, text='Marca:', background='Silver', foreground='black', anchor=W)
+    # nome mobile:
+    lb2 = Label(app3, text='Equipamento:', background='Silver', foreground='black', anchor=W)
     lb2.place(x=10, y=90, height=20, width=100)
 
-    marca_mobile = Entry(app3)
-    marca_mobile.place(x=120, y=90, height=20, width=350)
+    nome_mobile = Label(app3, text=f'{obj_mob}', background='Silver', foreground='black', anchor=W)
+    nome_mobile.place(x=120, y=90, height=20, width=350)
 
-    # modelo
-    lb3 = Label(app3, text='Modelo:', background='Silver', foreground='black', anchor=W)
+    # marca
+    lb3 = Label(app3, text='Marca:', background='Silver', foreground='black', anchor=W)
     lb3.place(x=10, y=120, height=20, width=100)
 
-    modelo_mobile = Entry(app3)
-    modelo_mobile.place(x=120, y=120, height=20, width=350)
+    marca_mobile = Entry(app3)
+    marca_mobile.place(x=120, y=120, height=20, width=350)
 
-    # cod_chip
-    lb4 = Label(app3, text='Código do Chip:', background='Silver', foreground='black', anchor=W)
+    # modelo
+    lb4 = Label(app3, text='Modelo:', background='Silver', foreground='black', anchor=W)
     lb4.place(x=10, y=150, height=20, width=100)
 
+    modelo_mobile = Entry(app3)
+    modelo_mobile.place(x=120, y=150, height=20, width=350)
+
+    # cod_chip
+    lb5 = Label(app3, text='Código do Chip:', background='Silver', foreground='black', anchor=W)
+    lb5.place(x=10, y=180, height=20, width=100)
+
     cod_chip_mobile = Entry(app3)
-    cod_chip_mobile.place(x=120, y=150, height=20, width=350)
+    cod_chip_mobile.place(x=120, y=180, height=20, width=210)
+
+    # comando para exibir numero do chip atrelado ao codigo
+    def comando_btn34():
+        codigo_chip = cod_chip_mobile.get()
+        detectar_chip_ind = plan0_chip[plan0_chip['cod_chip'] == codigo_chip].index.tolist()
+        if not detectar_chip_ind:
+            return num_chip_mob.configure(text='Chip não cadastrado')
+        numero_chip = plan0_chip.iloc[detectar_chip_ind[0]]['numero']
+        num_chip_mob.configure(text=numero_chip)
+
+    # Botão para exibir numero do chip atrelado ao codigo
+    btn34 = Button(app3, text='Exibir nº do CHIP', command=comando_btn34)
+    btn34.place(x=350, y=180, height=20, width=120)
 
     # numero chip
-    lb7 = Label(app3, text='Número do Chip:', background='Silver', foreground='black', anchor=W)
-    lb7.place(x=10, y=210, height=20, width=100)
+    lb6 = Label(app3, text='Número do Chip:', background='Silver', foreground='black', anchor=W)
+    lb6.place(x=10, y=210, height=20, width=100)
 
     num_chip_mob = Label(app3, text=' ', background='Silver', foreground='black', anchor=W)
     num_chip_mob.place(x=120, y=210, height=20, width=350)
@@ -92,15 +122,15 @@ def cadastro_mobile(cod_mob, obj_mob):
                modelo_mobile_lista[0] + " - " + \
                cod_chip_mobile_lista[0]
 
-        lb5.configure(text=hist, background='Black', foreground='white')
+        lb7.configure(text=hist, background='Gray')
         btn32.configure(command=comando_btn32)
         btn33.configure(command=comando_btn33)
 
     btn31 = Button(app3, text='Carregar Dados', command=comando_btn31)
-    btn31.place(x=100, y=170, height=40, width=150)
+    btn31.place(x=175, y=250, height=40, width=150)
 
-    lb5 = Label(app3, text=' ', background='Silver', foreground='black', anchor=W)
-    lb5.place(x=50, y=250, height=40, width=400)
+    lb7 = Label(app3, text=' ', background='silver', foreground='black')
+    lb7.place(x=50, y=320, height=40, width=400)
 
     def comando_btn32():
         if len(cod_mobile_lista) == 0:
@@ -121,24 +151,16 @@ def cadastro_mobile(cod_mob, obj_mob):
         marca_mobile.delete(0, "end")
         modelo_mobile.delete(0, "end")
         cod_chip_mobile.delete(0, "end")
+        btn33.configure(command=msg_erro_apagar)
 
     btn32 = Button(app3, text='Cadastrar', command=msg_erro_cadastro)
-    btn32.place(x=50, y=350, height=40, width=150)
+    btn32.place(x=50, y=390, height=40, width=150)
 
     btn33 = Button(app3, text='Apagar', command=msg_erro_apagar)
-    btn33.place(x=275, y=350, height=40, width=150)
+    btn33.place(x=300, y=390, height=40, width=150)
 
-    # comando para exibir numero do chip atrelado ao codigo
-    def comando_btn34():
-        codigo_chip = cod_chip_mobile.get()
-        detectar_chip_ind = plan0_chip[plan0_chip['cod_chip'] == codigo_chip].index.tolist()
-        if not detectar_chip_ind:
-            return num_chip_mob.configure(text='Chip não cadastrado')
-        numero_chip = plan0_chip.iloc[detectar_chip_ind[0]]['numero']
-        num_chip_mob.configure(text=numero_chip)
-
-    # Botão para exibir numero do chip atrelado ao codigo
-    btn34 = Button(app3, text='Atualizar CHIP', command=comando_btn34)
-    btn34.place(x=300, y=350, height=40, width=150)
 
     app3.mainloop()
+
+if __name__ == '__main__':
+    cadastro_mobile('MO10', 'celular')
